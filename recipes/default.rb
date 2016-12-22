765f8b650675e64c16dfb2219835c92ccdd7429e
poladinesh <dp.websphere@gmail.com>
2016-12-21 22:17:48 -0600

    second version of mycookbook1

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,6 @@ service 'httpd' do
   action [:enable, :start]
 end
 
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-end
+# template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+# end
