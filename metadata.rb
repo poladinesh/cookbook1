d49316100245fa5fd4979080dd1cec5a0aa090af
poladinesh <dp.websphere@gmail.com>
2016-12-21 22:13:14 -0600

    first version of mycookbook1

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'mycookbook'
+maintainer 'dinesh'
+maintainer_email 'dp.websphere@gmail.com'
+license 'all_rights'
+description 'Installs/Configures httpd webserver'
+long_description 'Installs/Configures mycookbook'
+version '0.3.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/mycookbook/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/mycookbook' if respond_to?(:source_url)
